0e57bc695cfe1b0f6a46572be1381286990bae45
markodufala <marko.dufala@studentstc.sk>
2021-10-14 20:21:56 +0200

    Changes in main code

--- build/index.js
+++ build/index.js
@@ -1,11 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/* .createInterface() a riadky po napísané pod tým sa
+používajú na inštanciu konfigurácie inputu a outputu */
 var intFace = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
-/* Vo vyšie napísanom kóde sa readline.createInterface() a riadky po napísané pod tým.
-používajú na inštanciu konfigurácie inputu a outputu */
+function sucet(a, b) {
+    var vysledok = (+a) + (+b); /* Vytvorím premennú vysledok, do ktorej priradíme čislo, ktoré vznikne na základe vykonanej operácie, ktorú sme si vyrbali a na základe inputu  */
+    // taktiež môžeme namiesto priradenia do premennej rovno vrátiť výsledok operácie, čiže: return (+a) + (+b);
+    return vysledok; // vraciame výsledok po operácii
+}
+function odcitanie(a, b) {
+    var vysledok = (+a) - (+b);
+    return vysledok;
+}
+function nasobenie(a, b) {
+    var vysledok = (+a) * (+b);
+    return vysledok;
+}
+function delenie(a, b) {
+    var vysledok = (+a) / (+b);
+    return vysledok;
+}
 function main() {
     /* metóda .question zobrazí náš dotaz v termináli a počká kým zadáme vstup, ktorý priradí do danej premennej.
     Následne sa presunie ku dalšej metóde ktora znova priradí input to premenej. A to celé sa opakuje ešte raz. */
@@ -14,26 +31,21 @@ function main() {
             intFace.question("Zadajte váš operátor:", function (druheCislo) {
                 /* V tomto štádiu uź máme všetky inputy, ktoré potrebujeme
                 pre výpočet uložené do premenných a môžeme začať počítať. */
-                if (operator === "+") { /* porovnávame našu premennú operator s matematickými znamienkami, ak sa to
-                                        znamienko rovná +,-,*,/ tak sa vykonajú operácie pod tým  */
-                    var vysledok = (+prveCislo) + (+druheCislo); /* Vytvorím premennú vysledok, do ktorej priradíme čislo,
-                                                                    ktoré vznikne na základe vykonanej operácie, ktorú sme si vyrbali */
-                    console.log("S\u00FA\u010Det t\u00FDchto \u010D\u00EDsel je " + vysledok); // vypíšeme do console, náš výsledok,
+                if (operator === "+") { /* porovnávame našu premennú operator s matematickými znamienkami, ak sa to znamienko rovná +,-,*,/ tak sa vykonajú operácie pod tým  */
+                    // vypíšeme do console, náš výsledok,
+                    console.log("S\u00FA\u010Det t\u00FDchto \u010D\u00EDsel je:", sucet(prveCislo, druheCislo));
                     intFace.close(); // ukončenie programu a prijímania inputu
                 }
                 else if (operator === "-") {
-                    var vysledok = (+prveCislo) - (+druheCislo);
-                    console.log("Od\u010D\u00EDtanie t\u00FDchto \u010D\u00EDsel je " + vysledok);
+                    console.log("Od\u010D\u00EDtanie t\u00FDchto \u010D\u00EDsel je:", odcitanie(prveCislo, druheCislo));
                     intFace.close();
                 }
                 else if (operator === "*") {
-                    var vysledok = (+prveCislo) * (+druheCislo);
-                    console.log("N\u00E1sobenie t\u00FDchto \u010D\u00EDsel je " + vysledok);
+                    console.log("N\u00E1sobenie t\u00FDchto \u010D\u00EDsel je:", nasobenie(prveCislo, druheCislo));
                     intFace.close();
                 }
                 else if (operator === "/") {
-                    var vysledok = (+prveCislo) / (+druheCislo);
-                    console.log("Delenie t\u00FDchto \u010D\u00EDsel je " + vysledok);
+                    console.log("Delenie t\u00FDchto \u010D\u00EDsel je:", delenie(prveCislo, druheCislo));
                     intFace.close();
                 }
                 /* ak nebolo zadané správne znamienko na matematickú operáciu
--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
 import readline from 'readline';
 
+/* .createInterface() a riadky po napísané pod tým sa
+používajú na inštanciu konfigurácie inputu a outputu */
+
 const intFace = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-/* Vo vyšie napísanom kóde sa readline.createInterface() a riadky po napísané pod tým.
-používajú na inštanciu konfigurácie inputu a outputu */
+function sucet(a: number, b: number): number {  // funkcia zoberie cislo A a cislo B a vráti nám výsledok ako číslo
+    var vysledok = (+a) + (+b); /* Vytvorím premennú vysledok, do ktorej priradíme čislo, ktoré vznikne na základe vykonanej operácie, ktorú sme si vyrbali a na základe inputu  */
 
+    // taktiež môžeme namiesto priradenia do premennej rovno vrátiť výsledok operácie, čiže: return (+a) + (+b);
+    return vysledok // vraciame výsledok po operácii
+}
 
+function odcitanie(a: number, b: number): number {
+    var vysledok = (+a) - (+b); 
+    return vysledok
+}
+
+function nasobenie(a: number, b: number): number {
+    var vysledok = (+a) * (+b); 
+    return vysledok
+}
+
+function delenie(a: number, b: number): number {
+    var vysledok = (+a) / (+b); 
+    return vysledok
+}
 
 function main(): void{ /* Aby sme povedali že táto hlavná funkcia nebude nič vraciať
                      ako výsledok, tak za názovFunkcie(): napíšeme void, čo znači že nám nič nevráti */
@@ -17,31 +37,25 @@ function main(): void{ /* Aby sme povedali že táto hlavná funkcia nebude nič
     Následne sa presunie ku dalšej metóde ktora znova priradí input to premenej. A to celé sa opakuje ešte raz. */
     intFace.question("Zadajte prvé číslo: ", (prveCislo: number) => { 
         intFace.question("Zadajte váš operátor: (+,-,*,/)", (operator: string)=> {
-            intFace.question("Zadajte váš operátor:", (druheCislo: number)=> {
-            
+            intFace.question("Zadajte váš operátor:", (druheCislo: number)=> {  
                 /* V tomto štádiu uź máme všetky inputy, ktoré potrebujeme 
                 pre výpočet uložené do premenných a môžeme začať počítať. */
 
-            if (operator ==="+"){  /* porovnávame našu premennú operator s matematickými znamienkami, ak sa to 
-                                    znamienko rovná +,-,*,/ tak sa vykonajú operácie pod tým  */
-                var vysledok = (+prveCislo) + (+druheCislo); /* Vytvorím premennú vysledok, do ktorej priradíme čislo, 
-                                                                ktoré vznikne na základe vykonanej operácie, ktorú sme si vyrbali */
-                console.log(`Súčet týchto čísel je ${vysledok}`); // vypíšeme do console, náš výsledok,
+            if (operator ==="+"){  /* porovnávame našu premennú operator s matematickými znamienkami, ak sa to znamienko rovná +,-,*,/ tak sa vykonajú operácie pod tým  */
+                // vypíšeme do console, náš výsledok,
+                console.log(`Súčet týchto čísel je:`, sucet(prveCislo,druheCislo));
                 intFace.close(); // ukončenie programu a prijímania inputu
             }
             else if (operator ==="-"){
-                var vysledok = (+prveCislo) - (+druheCislo);
-                console.log(`Odčítanie týchto čísel je ${vysledok}`);
+                console.log(`Odčítanie týchto čísel je:`, odcitanie(prveCislo,druheCislo));
                 intFace.close();
             }
             else if (operator ==="*"){
-                var vysledok = (+prveCislo) * (+druheCislo);
-                console.log(`Násobenie týchto čísel je ${vysledok}`);
+                console.log(`Násobenie týchto čísel je:`, nasobenie(prveCislo,druheCislo));
                 intFace.close();
             }
             else if (operator ==="/"){
-                var vysledok = (+prveCislo) / (+druheCislo);
-                console.log(`Delenie týchto čísel je ${vysledok}`);
+                console.log(`Delenie týchto čísel je:`, delenie(prveCislo,druheCislo));
                 intFace.close();
             }
             /* ak nebolo zadané správne znamienko na matematickú operáciu 
@@ -53,8 +67,5 @@ function main(): void{ /* Aby sme povedali že táto hlavná funkcia nebude nič
             });
         });
     });
-    
 }
-
-
 main();
