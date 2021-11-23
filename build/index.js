e54be470e430b7292183945e245eb3f259424117
markodufala <marko.dufala@studentstc.sk>
2021-11-23 20:29:59 +0100

    session4

--- build/index.js
+++ build/index.js
@@ -8,12 +8,8 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = require("body-parser");
 const http_1 = __importDefault(require("http"));
-const myJSON = JSON.parse(fs_1.default.readFileSync("src/books.json").toString());
+//const myJSON: Book[] = JSON.parse(fs.readFileSync("src/books.json").toString());
 let app;
-let myMap = new Map();
-myJSON.forEach((book) => {
-    myMap.set(book.id, book);
-});
 function createserver() {
     app = (0, express_1.default)();
     app.use((0, cors_1.default)());
@@ -23,25 +19,71 @@ function createserver() {
         console.log("Running server on port 3000");
     });
     app.get("/api/library/book/:id/info", (req, res) => {
-        let _id = parseInt(req.params["id"]);
-        let book = myMap.get(_id);
+        const myJSON = JSON.parse(fs_1.default.readFileSync("src/books.json").toString());
+        let myMap = new Map();
+        myJSON.forEach((book) => {
+            myMap.set(book.id, book);
+        });
+        const _id = parseInt(req.params["id"]);
         if (myMap.has(_id)) {
-            console.log("The id of your book is " + _id + ". The name of the book is: " + book.name);
-            res.json({ id: book.id, name: book.name, author: book.author, genre: book.genre });
+            const book = myMap.get(_id);
+            console.log("The id of your book is " +
+                _id +
+                ". The name of the book is: " +
+                book.name);
+            res.json({
+                id: book.id,
+                name: book.name,
+                author: book.author,
+                genre: book.genre,
+            });
         }
         else {
             res.json({ Response: "There is no book in the json with this ID" });
         }
     });
     app.post("/api/library/book/:id/info", (req, res) => {
-        let _id = parseInt(req.params["id"]);
-        let book = myMap.get(_id);
-        console.log(_id);
+        const myJSON = JSON.parse(fs_1.default.readFileSync("src/books.json").toString());
+        let myMap = new Map();
+        myJSON.forEach((book) => {
+            myMap.set(book.id, book);
+        });
+        // console.log(myJSON);
+        const _id = parseInt(req.params["id"]);
         if (myMap.has(_id)) {
+            const book = myMap.get(_id);
             res.json(book);
         }
         else {
             res.json({ Response: "There is no book in the json with this ID" });
+        }
+    });
+    app.put("/api/library/book/:id/add", (req, res) => {
+        const myJSON = JSON.parse(fs_1.default.readFileSync("src/books.json").toString());
+        let myMap = new Map();
+        myJSON.forEach((book) => {
+            myMap.set(book.id, book);
+        });
+        console.log(req.body);
+        const _id = parseInt(req.params["id"]);
+        if (myMap.has(_id)) {
+            res.json({ Response: "There is already book in the json with this ID" });
+        }
+        else {
+            myJSON.push({
+                id: req.body["id"],
+                name: req.body["name"],
+                author: req.body["author"],
+                genre: req.body["genre"],
+                year_published: req.body["published"],
+                publisher: req.body["publisher"],
+                country: req.body["origin_country"],
+                number_of_pages: req.body["pages"],
+            });
+            let json = JSON.stringify(myJSON);
+            fs_1.default.writeFile("src/books.json", json, "utf8", function (err) {
+                res.json({ Response: "The book was added" });
+            });
         }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,98 @@
-import fs from "fs"
-import {Book} from "./books"
-import express from "express"
-import cors from "cors"
-import bodyParser, { json, urlencoded} from "body-parser"
-import http from "http"
+import fs from "fs";
+import { Book } from "./books";
+import express from "express";
+import cors from "cors";
+import bodyParser, { json, urlencoded } from "body-parser";
+import http from "http";
 
-
-const myJSON: Book[] = JSON.parse(fs.readFileSync("src/books.json").toString())
-let app
-
-let myMap = new Map()
-myJSON.forEach((book: Book) => {
-    myMap.set(book.id,<Book>book)
-})
+//const myJSON: Book[] = JSON.parse(fs.readFileSync("src/books.json").toString());
+let app;
 
 function createserver() {
-    app = express()
-    app.use(cors())
-    app.use(json())
-    app.use(urlencoded({extended: false}))
+  app = express();
+  app.use(cors());
+  app.use(json());
+  app.use(urlencoded({ extended: false }));
 
-    http.createServer(app).listen(3000, () => {
-        console.log("Running server on port 3000")
-    })
+  http.createServer(app).listen(3000, () => {
+    console.log("Running server on port 3000");
+  });
 
-    app.get("/api/library/book/:id/info", (req, res) => {
-        const _id = parseInt(req.params["id"])
-        const book = myMap.get(_id)
-        if (myMap.has(_id)) {
-            console.log("The id of your book is " + _id + ". The name of the book is: " + book.name)
-            res.json({id: book.id, name: book.name, author: book.author, genre: book.genre})
-        } else {
-            res.json({Response: "There is no book in the json with this ID"})
-        }
-    })
+  app.get("/api/library/book/:id/info", (req, res) => {
+    const myJSON: Book[] = JSON.parse(
+      fs.readFileSync("src/books.json").toString()
+    );
+    let myMap = new Map();
+    myJSON.forEach((book: Book) => {
+      myMap.set(book.id, <Book>book);
+    });
+    const _id = parseInt(req.params["id"]);
+    if (myMap.has(_id)) {
+      const book = myMap.get(_id);
+      console.log(
+        "The id of your book is " +
+          _id +
+          ". The name of the book is: " +
+          book.name
+      );
+      res.json({
+        id: book.id,
+        name: book.name,
+        author: book.author,
+        genre: book.genre,
+      });
+    } else {
+      res.json({ Response: "There is no book in the json with this ID" });
+    }
+  });
 
-    app.post("/api/library/book/:id/info", (req, res) => {
-        const _id = parseInt(req.params["id"])
-        const book = myMap.get(_id)
-        if (myMap.has(_id)) {
-            res.json(book)
-        } else {
-            res.json({Response: "There is no book in the json with this ID"})
-        }
-    })
+  app.post("/api/library/book/:id/info", (req, res) => {
+    const myJSON: Book[] = JSON.parse(
+      fs.readFileSync("src/books.json").toString()
+    );
+    let myMap = new Map();
+    myJSON.forEach((book: Book) => {
+      myMap.set(book.id, <Book>book);
+    });
+    // console.log(myJSON);
+    const _id = parseInt(req.params["id"]);
+    if (myMap.has(_id)) {
+      const book = myMap.get(_id);
+      res.json(book);
+    } else {
+      res.json({ Response: "There is no book in the json with this ID" });
+    }
+  });
+
+  app.put("/api/library/book/:id/add", (req, res) => {
+    const myJSON: Book[] = JSON.parse(
+      fs.readFileSync("src/books.json").toString()
+    );
+    let myMap = new Map();
+    myJSON.forEach((book: Book) => {
+      myMap.set(book.id, <Book>book);
+    });
+    console.log(req.body);
+    const _id = parseInt(req.params["id"]);
+    if (myMap.has(_id)) {
+      res.json({ Response: "There is already book in the json with this ID" });
+    } else {
+      myJSON.push({
+        id: req.body["id"],
+        name: req.body["name"],
+        author: req.body["author"],
+        genre: req.body["genre"],
+        year_published: req.body["published"],
+        publisher: req.body["publisher"],
+        country: req.body["origin_country"],
+        number_of_pages: req.body["pages"],
+      });
+
+      let json = JSON.stringify(myJSON);
+      fs.writeFile("src/books.json", json, "utf8", function (err) {
+        res.json({ Response: "The book was added" });
+      });
+    }
+  });
 }
-createserver()
+createserver();
